77aa4c1a77a6787586323ab49ffeafebe874b632
Mukund <mukund06.venkatesh@gmail.com>
2022-03-02 17:53:22 -0500

    aight so js done fricking finally

--- script.js
+++ script.js
@@ -23,3 +23,166 @@ function initBoard() {
 
 initBoard()
 
+document.addEventListener('keyup', (e) => {
+
+    if (guessesRemaining === 0) {
+        return
+    }
+
+    let pressedKey = String(e.key)
+    if (pressedKey === "Backspsace" && nextLetter !== 0) {
+        deleteLetter()
+        return
+    }
+
+    if (pressedKey === "Enter") {
+        checkGuess()
+        return
+    }
+
+    let found = pressedKey.match(/[a-z]/gi)
+    if (!found || found.length > 1) {
+        return
+    } else {
+        insertLetter(pressedKey)
+    }
+})
+
+function insertLetter (pressedKey) {
+    if (nextLetter === 5) {
+        return
+    }
+    pressedKey = pressedKey.toLowerCase()
+
+    let row = document.getElementsByClassName('letter-row')[6-guessesRemaining]
+    let box = row.children[nextLetter]
+    animateCSS(box,"pulse")
+    box.textContent = pressedKey
+    box.classList.add('filled-box')
+    currentGuess.push(pressedKey)
+    nextLetter+=1
+}
+
+function deleteLetter () {
+    let row = document.getElementsByClassName('letter-row')[6-guessesRemaining]
+    let box = row.children[nextLetter-1]
+    box.textContent = ""
+    box.classList.remove('filled-box')
+    currentGuess.pop()
+    nextLetter-=1
+}
+
+function checkGuess ()  {
+    let row = document.getElementsByClassName("letter-row")[6-guessesRemaining]
+    let guessString = ''
+    let rightGuess = Array.from(rightGuessString)
+
+    for (const val of currentGuess) {
+        guessString += val
+    }
+
+    if (guessString.length !=5) {
+        toastr.error("Not Enough Letters bruh")
+        return
+    }
+
+    if (!WORDS.includes(guessString)) {
+        toastr.error("Word not in list")
+        return
+    }
+
+    for (let i = 0; i < 5; i++) {
+        let letterColor = ""
+        let box = row.children[i]
+        let letter = currentGuess[i]
+
+        let letterPosition = rightGuess.indexOf(currentGuess[i])
+        if (letterPosition === -1) {
+            letterColor = 'grey'
+
+        } else {
+            if (currentGuess[i] === rightGuess[i]) {
+                letterColor = 'green'
+            } else {
+                letterColor = 'yellow'
+            }
+
+            rightGuess[letterPosition] = "#"
+        }
+
+        let delay = 250*i
+        setTimeout(()=> {
+            animateCSS(box,"flipInX")
+            box.style.backgroundColor = letterColor
+            shadeKeyBoard(letter,letterColor)
+        },delay)
+    }
+
+    if (guessString === rightGuessString) {
+        toastr.success("You guessed right. Game Over boiz")
+        guessesRemaining = 0
+        return
+    } else {
+        guessesRemaining -=1
+        currentGuess = []
+        nextLetter = 0
+        
+        if (guessesRemaining===0) {
+            toastr.error("You ran out of guesses L bozo")
+            toastr.info(`The right word was: "${rightGuessString}"`)
+        }
+    }
+}
+
+function shadeKeyBoard(letter,color) {
+    for (const elem of document.getElementsByClassName('keyboard-button')) {
+        if (elem.textContent === letter){
+            let oldColor = elem.style.backgroundColor
+            if (oldColor === 'green') {
+                return
+            }
+
+            if (oldColor === 'yellow' && color !== 'green') {
+                return
+            }
+
+            elem.style.backgroundColor = color
+            break
+        }
+    }
+}
+
+
+
+document.getElementById("keyboard-cont").addEventListener("click", (e)=> {
+    const target = e.target
+    if (target.classList.contains("keyboard-button")) {
+        return
+    }
+    let key = target.textContent
+
+    if (key === "Del") {
+        key = "Backspace"
+    }
+
+    document.dispatchEvent(new KeyboardEvent("keyup", {'key':key}))
+
+})
+
+
+const animateCSS = (element, animation, prefix = 'animate__') =>
+    new Promise((resolve,reject) => {
+        const animationName = `${prefix}${animation}`;
+        const node = element
+        node.style.setPropertu('--animate-duration', '0.3s');
+
+        node.classList.add(`${prefix}animated`,animationName);
+
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            node.classList.remove(`${prefix}animated`,animationName);
+            resolve('Animation Ended');
+        }
+
+        node.addEventListener('animationend',handleAnimationEnd, {once:true})
+    })
