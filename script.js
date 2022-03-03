e6c71161f571204374db75fd16fe531dc0b54a73
Mukund676 <mukund06.venkatesh@gmail.com>
2022-03-03 07:21:24 -0500

    lighter green

--- script.js
+++ script.js
@@ -29,11 +29,11 @@ function shadeKeyBoard(letter, color) {
     for (const elem of document.getElementsByClassName("keyboard-button")) {
         if (elem.textContent === letter) {
             let oldColor = elem.style.backgroundColor
-            if (oldColor === 'green') {
+            if (oldColor === 'lightGreen') {
                 return
             } 
 
-            if (oldColor === 'yellow' && color !== 'green') {
+            if (oldColor === 'yellow' && color !== 'lightGreen') {
                 return
             }
 
@@ -83,7 +83,7 @@ function checkGuess () {
             letterColor = 'grey'
         } else { 
             if (currentGuess[i] === rightGuess[i]) {
-                letterColor = 'green'
+                letterColor = 'lightGreen'
             } else {
                 letterColor = 'yellow'
             }
